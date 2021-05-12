0ee498f1f4275e7d830599013f27985c5a041972
Mostafa Elbannan <mostafaelbannan1@gmail.com>
2021-05-12 09:22:42 -0600

    added minification script

---
+++ buildSetup.js
@@ -0,0 +1,22 @@
+const fs = require(`fs`)
+
+const targetDirectory = `./dist`
+
+if (!fs.existsSync(targetDirectory)) {
+    fs.mkdirSync(targetDirectory)
+}
+
+const targetFilesToCopy = [`index.html`, `timer.mp3`, `favicon.ico`]
+const copyFileToDestination = (filename) => {
+    const targetDestination = targetDirectory + `/${filename}`
+    return fs.copyFile(filename, targetDestination, (err) => {
+        if (err) {
+            console.error(err)
+            throw err
+        } else {
+            console.log(`Successfully copied file: ${filename} to ${targetDirectory}`)
+        }
+    })
+}
+
+targetFilesToCopy.forEach(f => copyFileToDestination(f))
